78b67fecab3cc07308a495f06c0c89fffb4dd3f4
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-08 18:19:24 -0300

    :sparkles: add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, presence: true
+  validates :profile, presence: true
+
+  enum profile: { admin: 0, client: 1 }
 end
