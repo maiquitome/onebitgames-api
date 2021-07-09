8ae4cc4f9b094490033e55e48eb0935b41664e05
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-09 11:07:59 -0300

    :sparkles: using the customised validation for the future date

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,6 @@
 class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false }
-  validates :due_date, presence: true
+  validates :due_date, presence: true, future_date: true
   validates :discount_value, presence: true, numericality: { greater_than: 0 }
   validates :status, presence: true
   enum status: { active: 1, inactive: 2 }
