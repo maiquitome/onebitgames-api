e13446222c87c8aa491e0bda5d8246424faa31e8
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-08 21:58:32 -0300

    :sparkles: add Coupon model

---
+++ app/models/coupon.rb
@@ -0,0 +1,7 @@
+class Coupon < ApplicationRecord
+  validates :code, presence: true, uniqueness: { case_sensitive: false }
+  validates :due_date, presence: true
+  validates :discount_value, presence: true, numericality: { greater_than: 0 }
+  validates :status, presence: true
+  enum status: { active: 1, inactive: 2 }
+end
---
+++ spec/factories/coupons.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :coupon do
+    sequence(:name) { |n| "My Coupon #{n}" }
+    code { Faker::Commerce.unique.promotion_code(digits: 6) }
+    status { %i(active inactive).sample }
+    discount_value { rand(1..99) }
+    due_date { "2020-11-06 00:50:25" }
+  end
+end
