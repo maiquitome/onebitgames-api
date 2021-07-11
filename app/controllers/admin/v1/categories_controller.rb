6b9bcfd81ca1f9c9f54b00e13dc34e2c10bfc7b0
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-11 19:17:33 -0300

    :sparkles: add category registration and error rendering

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -3,5 +3,33 @@ module Admin::V1
     def index
       @categories = Category.all
     end
+
+    def create
+      @category = Category.new
+      @category.attributes = category_params
+
+      save_category!
+    rescue
+      # render json: { errors: {fields: @category.errors.messages} }, status: :unprocessable_entity
+      render_error(fields: @category.errors.messages)
+    end
+
+    private
+
+    # método que vai processar os parametros
+    def category_params
+      # retorna vazio se os params não tiverem a key category
+      # a ideia aqui é pra não quebrar após no require
+      return {} unless params.has_key?(:category)
+      # obriga a existir a categoria
+      params.require(:category).permit(:id, :name)
+    end
+
+    def save_category!
+      @category.save!
+      render :show
+    rescue
+      render_error(fields: @category.errors.messages)
+    end
   end
 end
